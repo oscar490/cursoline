b5f43ddd0abd6b121fd801d2bd4f9a74dc698a4f
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2018-06-21 18:09:59 +0200

    Creación de tabla cursos y modelo

--- db/cursoline.sql
+++ db/cursoline.sql
@@ -2,3 +2,19 @@
 -- Archivo de base de datos --
 ------------------------------
 
+
+-- Table cursos --
+
+DROP TABLE IF EXISTS cursos CASCADE;
+
+CREATE TABLE cursos
+(
+       id          BIGSERIAL    PRIMARY KEY
+    ,  nombre      VARCHAR(255) NOT NULL
+    ,  descripcion VARCHAR(255)
+);
+
+
+INSERT INTO cursos (nombre, descripcion)
+    VALUES ('Desarrollo de Aplicaciones Web', 'Descripcion de DAW'),
+            ('Sistemas Microiformáticos y Redes', 'Descripción de SMR');
---
+++ models/Cursos.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cursos".
+ *
+ * @property int $id
+ * @property string $nombre
+ * @property string $descripcion
+ */
+class Cursos extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cursos';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nombre'], 'required'],
+            [['nombre', 'descripcion'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nombre' => 'Nombre',
+            'descripcion' => 'Descripcion',
+        ];
+    }
+}
