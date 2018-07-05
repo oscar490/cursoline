eac5dcd75a8250e94cc0183f6369dba764c00612
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2018-07-05 12:44:32 +0200

    Se crea la tabla usuarios.

--- db/cursoline.sql
+++ db/cursoline.sql
@@ -2,6 +2,24 @@
 -- Archivo de base de datos --
 ------------------------------
 
+-- Tabla usuarios --
+
+DROP TABLE IF EXISTS usuarios CASCADE;
+
+CREATE TABLE usuarios
+(
+       id        BIGSERIAL    PRIMARY KEY
+    ,  nombre    VARCHAR(255) NOT NULL
+    ,  apellidos VARCHAR(255) NOT NULL
+    ,  email     VARCHAR(255) NOT NULL
+    ,  password  VARCHAR(255) NOT NULL
+    ,  token_act VARCHAR(255)
+    ,  token_rec VARCHAR(255)
+);
+
+INSERT INTO usuarios (nombre, apellidos, email, password)
+    VALUES ('Óscar', 'Vega Herrera', 'oscarvegaherrera59@gmail.com', 'unodostrescuatro');
+
 
 -- Tabla cursos --
 
